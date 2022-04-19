a963b07a660440018d3595b8534b0ff07f295fc5
Henrique <henrique.campana22@gmail.com>
2022-04-19 16:37:35 -0300

    Requisito 1 completo

---
+++ src/components/Table.jsx
@@ -0,0 +1,60 @@
+import React, { useContext } from 'react';
+import MyContext from '../context/MyContext';
+
+function Table() {
+  const { data } = useContext(MyContext);
+  const tableColumns = [
+    'Name',
+    'Rotation Period',
+    'Orbital Period',
+    'Diameter',
+    'Climate',
+    'Gravity',
+    'Terrain',
+    'Surface Water',
+    'Population',
+    'Films',
+    'Created',
+    'Edited',
+    'Url',
+  ];
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          { tableColumns.map((item) => (
+            <th key={ item }>
+              { item }
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        { data.map((planet) => (
+          <tr key={ planet.name }>
+            <td>{ planet.name }</td>
+            <td>{ planet.rotation_period }</td>
+            <td>{ planet.orbital_period }</td>
+            <td>{ planet.diameter }</td>
+            <td>{ planet.climate }</td>
+            <td>{ planet.gravity }</td>
+            <td>{ planet.terrain }</td>
+            <td>{ planet.surface_water }</td>
+            <td>{ planet.population }</td>
+            <td>
+              { planet.films.map((film) => (
+                <p key={ film }>{ film }</p>
+              ))}
+            </td>
+            <td>{ planet.created }</td>
+            <td>{ planet.edited }</td>
+            <td>{ planet.url }</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
